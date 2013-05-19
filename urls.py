eb4d93c86ec78528a40e06d587766d66454c8089
ianluo <ianluo63@gmail.com>
2013-05-19 22:33:59 +0800

    change sub url

--- urls.py
+++ urls.py
@@ -7,9 +7,9 @@ import views
 
 urlpatterns = patterns('',
     # Examples:
-     url(r'^newapps/+$', views.get_new_apps),
-     url(r'^newapps/appdetail/(?P<app_id>\d*)$',views.get_app_detail),
-     url(r'^newapps/applist/(?P<list_type>\w*)/(?P<genre>\w*)$',views.get_app_list),
+     url(r'^ios/+$', views.get_new_apps),
+     url(r'^ios/appdetail/(?P<app_id>\d*)$',views.get_app_detail),
+     url(r'^ios/applist/(?P<list_type>\w*)/(?P<genre>\w*)$',views.get_app_list),
     # url(r'^jaguar/', include('jaguar.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
